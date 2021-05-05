81e75583d403cfe645e132b07592e8b90f1acae5
Allan Bennett <allanbts.18music@gmail.com>
2021-05-05 11:00:03 -0500

    Virtualkeyboard, shift and backspace icons updated

--- Controller_Interface/src/visual_classes/VirtualKeyboard.java
+++ Controller_Interface/src/visual_classes/VirtualKeyboard.java
@@ -63,8 +63,8 @@ public class VirtualKeyboard implements FocusListener {
     // Special keys definition
     //private final Key TAB_KEY = new Key(KeyEvent.VK_TAB, "Tab");
     private final Key PARENTHESIS_KEY = new Key(KeyEvent.VK_LEFT_PARENTHESIS,"(",")");
-    private final Key CAPS_LOCK_KEY = new Key(KeyEvent.VK_CAPS_LOCK, "Caps Lock");
-    private final Key SHIFT_KEY = new Key(KeyEvent.VK_SHIFT, "Shift");
+    private final Key CAPS_LOCK_KEY = new Key(KeyEvent.VK_CAPS_LOCK, "Caps");
+    private final Key SHIFT_KEY = new Key(KeyEvent.VK_SHIFT, "⬆");
     private final Key ACUTE_KEY = new Key(KeyEvent.VK_DEAD_ACUTE, "´", "`");
     private final Key GRAVE_KEY = new Key(KeyEvent.VK_DEAD_GRAVE, "`");
     private final Key TILDE_CIRCUMFLEX_KEY = new Key(KeyEvent.VK_DEAD_TILDE, "~", "^");
@@ -79,7 +79,7 @@ public class VirtualKeyboard implements FocusListener {
         new Key(KeyEvent.VK_7, "7"), new Key(KeyEvent.VK_8, "8"),
         new Key(KeyEvent.VK_9, "9"), new Key(KeyEvent.VK_0, "0"),
         new Key(KeyEvent.VK_MINUS, "-", "_"),
-        new Key(KeyEvent.VK_BACK_SPACE, "<<<")
+        new Key(KeyEvent.VK_BACK_SPACE, "⟵")
     };
 
     // Second key row
@@ -118,11 +118,6 @@ public class VirtualKeyboard implements FocusListener {
         new Key(KeyEvent.VK_SEMICOLON, ";", ":"),
         new Key(KeyEvent.VK_SLASH, "/", "?")
     };
-
-    // Fifth key row (spacebar only)
-  /*  private Key[] row5 = new Key[]{
-        new Key(KeyEvent.VK_SPACE, " ")
-    }; */
 
     private final Map<Key, JButton> buttons;
     private Component currentComponent;
