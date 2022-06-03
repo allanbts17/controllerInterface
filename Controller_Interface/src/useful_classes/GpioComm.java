cb050bec57b75fbf613c02f9db18e004f5168bc8
Allan <allanbts.18music@gmail.com>
2022-06-02 20:23:43 -0500

    pi4j fixed

--- Controller_Interface/src/useful_classes/GpioComm.java
+++ Controller_Interface/src/useful_classes/GpioComm.java
@@ -1,9 +1,18 @@
 package useful_classes;
+
+import com.pi4j.Pi4J;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.io.gpio.digital.PullResistance;
+import com.pi4j.util.Console;
+
+/* 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.RaspiPin;*/
 
 public class GpioComm {
 	// crear controlador gpio
