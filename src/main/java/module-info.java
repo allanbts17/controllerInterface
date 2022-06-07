fdc408e0044eb1c1477dbceb8ec7d451c861bcda
Allan <allanbts.18music@gmail.com>
2022-06-07 14:29:57 -0500

    gpio example integration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -32,6 +32,8 @@ module com.pi4j.example {
     // SLF4J MODULES
     requires org.slf4j;
     requires org.slf4j.simple;
+    requires java.desktop;
+    requires jlayer;
 
     uses com.pi4j.extension.Extension;
     uses com.pi4j.provider.Provider;
